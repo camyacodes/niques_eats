1bb0f4fb68d77e99ae5d4a341d2fd04f4ef86ecd
camyacodes <camyacodes@gmail.com>
2022-03-12 03:28:00 -0500

    added cart summary section

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,11 +1,14 @@
 import React from "react";
 import CheckoutInfo from "../components/checkout-info";
+import CartSummary from "../components/cart-summary";
 
 export default function Checkout() {
 	return (
 		
 			<body>
-				<CheckoutInfo />
+				
+			<CheckoutInfo />
+			<CartSummary />
 			</body>
 		
 	);
