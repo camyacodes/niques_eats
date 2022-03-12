e412c746724921ef4b459de0130faabe0cc5a687
chaudeveloper <ms_michanguyen@yahoo.com>
2022-03-12 16:13:58 -0500

    dinner menu styling

--- client/src/pages/MenuDinner.js
+++ client/src/pages/MenuDinner.js
@@ -15,7 +15,7 @@ const MenuDinner = () => {
     return (
 
 
-<div> 
+<div id="dinner-menu"> 
 
 <div id="title">
   <span >MENU </span>
@@ -45,12 +45,12 @@ const MenuDinner = () => {
 
 <div id="nav-buttons">
    <Row>
-     <Col xs="6" sm="4"> <h2 class="menu-nav-buttons">MAIN DISHES</h2></Col>
+     <Col xs="6" sm="4"> <h2 class="menu-nav-buttons-dinner">MAIN DISHES</h2></Col>
      <Col xs="6" sm="4"> <a href="#sides">
-     <h2 class="menu-nav-buttons">SIDES</h2>
+     <h2 class="menu-nav-buttons-dinner">SIDES</h2>
      </a></Col>
      <Col sm="4"><a href="#dessert">
-     <h2 class="menu-nav-buttons">DESSERT</h2>
+     <h2 class="menu-nav-buttons-dinner">DESSERT</h2>
      </a></Col>
    </Row>
    </div>
@@ -68,7 +68,7 @@ const MenuDinner = () => {
 
 
         {/* SIDES */}
-        <Col xs="6" sm="4"><h2 id="sides" class="active-menu  menu-nav-buttons">SIDES</h2></Col>
+        <Col xs="6" sm="4"><h2 id="sides" class="active-menu  menu-nav-buttons-dinner">SIDES</h2></Col>
         <div className='row row-cols-1 row-cols-md-3'>{Sides.map(photo => {return <Dish photo={photo}/>})}{Sides.map(photo => {return <Dish photo={photo}/>})}</div>
         
 
@@ -80,7 +80,7 @@ const MenuDinner = () => {
 
 
       {/* DESSERT */}
-      <Col xs="6" sm="4"><h2 id="dessert" class="active-menu menu-nav-buttons">DESSERT</h2></Col>
+      <Col xs="6" sm="4"><h2 id="dessert" class="active-menu menu-nav-buttons-dinner">DESSERT</h2></Col>
         <div className='row row-cols-1 row-cols-md-3'>{Desserts.map(photo => {return <Dish photo={photo}/>})}{Desserts.map(photo => {return <Dish photo={photo}/>})}</div>
       
 
