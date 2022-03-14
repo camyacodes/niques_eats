09c430f1a3a928182f56305d2c4068566141fccf
chaudeveloper <ms_michanguyen@yahoo.com>
2022-03-14 15:58:41 -0400

    working on dish card modal

--- client/src/components/Dish-card/index.js
+++ client/src/components/Dish-card/index.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button, Col, Row,
-  Container, Modal, ModalHeader, ModalBody, ModalFooter
+  Container, Modal, ModalHeader, ModalBody, ModalFooter, NavItem
 } from 'reactstrap';
-
+import Calendar from '../Calendar';
+import QtyPicker from 'react-quantity-picker';
+import "../../styles/modal.css"
 
 
 export default function Dish({ photo }) {
@@ -13,6 +15,15 @@ export default function Dish({ photo }) {
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const i = 1
+
+  const handleDecrement = (i) => {
+    this.i--
+  }
+
+  const handleIncrement = (i) => {
+    this.i++
+  }
 
 
 
@@ -30,64 +41,62 @@ export default function Dish({ photo }) {
 
             <CardBody>
               <div class="row align-items-center">
-                <CardTitle tag="h5" class="col">{photo.name}</CardTitle>
+                <CardTitle tag="h5" class="col text-start">{photo.name}</CardTitle>
                 <CardSubtitle tag="h5" className="col text-end">{photo.price}</CardSubtitle>
               </div>
-              <Button >Add to Cart</Button>
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Add to Cart
+              </button>
             </CardBody>
           </Card>
         </Col>
       </Container>
 
 
-      <div>
-			<button
-				type="button"
-				class="btn btn-primary"
-				data-bs-toggle="modal"
-				data-bs-target="#exampleModal"
-			>
-				Launch demo modal
-			</button>
+      <div class="dish-modal">
+        <div class="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
 
-			<div
-				class="modal fade"
-				id="exampleModal"
-				tabindex="-1"
-				aria-labelledby="exampleModalLabel"
-				aria-hidden="true"
-			>
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-header">
-							<h5 class="modal-title" id="exampleModalLabel">
-								Modal title
-							</h5>
-							<button
-								type="button"
-								class="btn-close"
-								data-bs-dismiss="modal"
-								aria-label="Close"
-							></button>
-						</div>
-                        <div class="modal-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </div>
-						<div class="modal-footer">
-							<button
-								type="button"
-								class="btn btn-secondary"
-								data-bs-dismiss="modal"
-							>
-								Close
-							</button>
-							<button type="button" class="btn btn-primary">
-								Save changes
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+              <div class="modal-header">
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+
+
+              <div class="modal-body">
+                <div class="row align-items-center">
+                  <CardImg top width="100%" src={photo.image} alt="Card image cap" id="dish-img" class="rounded mx-auto d-block" />
+                  <CardTitle tag="h5" class="col  text-start">{photo.name}</CardTitle>
+                  <CardSubtitle tag="h5" className="col text-end">{photo.price}</CardSubtitle>
+                </div>
+              </div>
+
+
+              <div class="modal-footer row">
+                <Calendar class="col-3"/>
+
+                <div className='nput-group col-6 row i'>
+                  <button type="button" onClick={() => handleDecrement()} className="input-group-text col-1">-</button>
+                  <div className='"form-control text-center col-1'>{i}</div>
+                  <button type="button" onClick={() => handleIncrement()} className="input-group-text col-1">+</button>
+                </div>
+
+                <button type="button" class="btn btn-primary col-5 m-3">Add to Cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
 
 
