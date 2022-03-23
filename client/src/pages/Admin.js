e6da96a0c33e474063a3c8b557960d49cd572b83
chaudeveloper <ms_michanguyen@yahoo.com>
2022-03-23 01:17:38 -0400

    starting admin panel

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -8,38 +8,29 @@ import About from "../components/About";
 import Contact from "../components/Contact";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
+import "../styles/admin.css"
 
 
 const Admin = () => {
     const userstate = useSelector((state) => state.loginUserReducer);
-    const { currentUser } = userstate;
+    const {currentUser} = userstate;
     const dispatch = useDispatch()
     useEffect(() => {
-        if (!currentUser.isAdmin) {
-            window.location.href = '/'
+        if(!currentUser.isAdmin){
+            window.location.href='/'
         }
     }, [])
-    return (
-        <div>
-            <h1>Admin Control Panel</h1>
-            <ul class="nav nav-tabs" id="myTab" role="tablist">
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Home</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Profile</button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Contact</button>
-                </li>
-            </ul>
-            <div class="tab-content" id="myTabContent">
-                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
-                <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
-                <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+       <h1>Admin Control Panel</h1>
+       <ul className="admin-nav">
+           <li><a href="">Users</a></li>
+           <li><a href="">Items</a></li>
+           <li><a href="">Add new item</a></li>
+           <li><a href="">Orders</a></li>     
+       </ul>
+    </div>
+  );
 };
 
 export default Admin;
