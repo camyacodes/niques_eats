7248e36ba76e0469c95fe0f1ca0dd73a50f09e70
chaudeveloper <ms_michanguyen@yahoo.com>
2022-03-23 14:02:29 -0400

    structure add-new-item section on admin panel need to route these inputs into database later

--- client/src/App.js
+++ client/src/App.js
@@ -45,7 +45,7 @@ const client = new ApolloClient({
 function App() {
   return (
 
-    <ApolloProvider client={client}>
+    <ApolloProvider client={client} id="content">
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
         <StoreProvider>
--- client/src/components/Admin/AddItem.js
+++ client/src/components/Admin/AddItem.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { Form } from "reactstrap";
 
-export default function AddItem() {
+ function AddItem() {
+    const[name, setname] = useState('')
+    const[price, setprice] = useState('')
+    const[image, setimage] = useState('')
+    const[description, setdescription] = useState('')
+
+    function formhandler(e){
+        e.preventDefault();
+        const product = {
+            name,
+            price,
+            image,
+            description
+        }
+        console.log (product);
+    }
+
+
+
+
     return (
-        <div>
-            Add Items
+        <div  className="position-relative">
+            <form onSubmit={formhandler} >
+                <input type="text" className="form-control text-center" placeholder="name" value={name} onChange={(e)=>{setname(e.target.value)}}  />
+                <input type="text" className="form-control text-center" placeholder="price" value={price} onChange={(e)=>{setprice(e.target.value)}}  />
+                <input type="text" className="form-control text-center" placeholder="image url" value={image} onChange={(e)=>{setimage(e.target.value)}}  />
+                <input type="text" className="form-control text-center" placeholder="description" value={description} onChange={(e)=>{setdescription(e.target.value)}}  />
+               <button className="btn mt-5 position-absolute top-100 start-50 translate-middle" type="submit">Add Item</button>
+            </form>
         </div>
     )
 }
+export default AddItem;
