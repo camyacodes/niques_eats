0947536faa289cdcd665a065152818d0253293e7
chaudeveloper <ms_michanguyen@yahoo.com>
2022-03-24 17:03:20 -0400

    adding responsiveness for home-info-cards

--- client/src/components/home-info-cards/index.js
+++ client/src/components/home-info-cards/index.js
@@ -1,45 +1,47 @@
 import React from "react";
+import './style.css';
 
 export default function HomeInfoCards() {
 	return (
-		<div>
+		
 			<div class="container info-container" id="home-info-cards">
 				{/* First row of info cards */}
-				<div class="row info-row">
-					<div class="col-sm info-section">
+				<div class="row info-row row-cols-1 row-cols-sm-2 row-cols-md-4 ">
+					<div class="col info-section">
 						<img
 							src="https://www.jessicagavin.com/wp-content/uploads/2018/06/ribeye-steak-with-red-wine-sauce-3-1200.jpg"
-							className="img-fluid info-img"
+							className="img-fluid info-img"						
 						/>
 					</div>
-					<div class="col-sm info-section">
-						<h2 className="info-text">Quality Ingedients</h2>
+					<div class="col info-section">
+						<h2 className="info-text right quanlity">Quality Ingedients</h2>
 					</div>
-					<div class="col-sm info-section">
+
+					<div class="col info-section">
 						<img
 							src="https://bakingmischief.com/wp-content/uploads/2021/02/banana-pudding-image-square.jpg"
 							className="img-fluid info-img"
 						/>
 					</div>
-					<div class="col-sm info-section">
-						<h2 className="info-text">Customizable</h2>
+					<div class="col info-section">
+						<h2 className="info-text right customizable">Customizable</h2>
 					</div>
 				</div>
 				{/* Second Row of Info Cards */}
-				<div class="row info-row">
-					<div class="col-sm info-section">
-						<h2 className="info-text">Southern Style</h2>
+				<div class="row info-row row-cols-1 row-cols-sm-2 row-cols-md-4 ">
+					<div class="col info-section">
+						<h2 className="info-text left southern">Southern Style</h2>
 					</div>
-					<div class="col-sm info-section">
+					<div class="col info-section">
 						<img
 							src="https://basilandbubbly.com/wp-content/uploads/2020/03/salmon-croquettes-9.jpg"
 							className="img-fluid info-img"
 						/>
 					</div>
-					<div class="col-sm info-section">
-						<h2 className="info-text">Convenient</h2>
+					<div class="col info-section">
+						<h2 class="info-text left convenient">Convenient</h2>
 					</div>
-					<div class="col-sm info-section">
+					<div class="col info-section">
 						<img
 							src="https://thecozycook.com/wp-content/uploads/2020/10/Buttermilk-Biscuits-f.jpg"
 							className="img-fluid info-img"
@@ -47,6 +49,6 @@ export default function HomeInfoCards() {
 					</div>
 				</div>
 			</div>
-		</div>
+		
 	);
 }
