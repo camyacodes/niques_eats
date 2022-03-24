2a263713a47b9a53ca8a17bfea6905dfcc644437
chaudeveloper <ms_michanguyen@yahoo.com>
2022-03-24 19:34:22 -0400

    about section on home page styled and responsive

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import profile from "../../assets/profile.jpg"
+import './style.css'
 
 const About = () => {
   return (
     
-      <div class="row g-0">
+      <div class="row g-0" id="about-section">
 
 
-        <div class="col-md-4">
-        <img src={ profile } class="img-fluid rounded-3 p-4"  alt="owner of Nique's Eats" />
+        <div class="col-md-4" id="profile">
+        <img src={ profile } class="img-fluid rounded-3 p-4"  alt="owner of Nique's Eats" id="profile-img" />
         </div>
 
-       <div class="col-md-8">
+       <div class="col-md-8" id="about-text">
            <div class="card-body">
        <h5 class="card-title text-center p-4" id="about">ABOUT ME</h5>
        <p class="card-text about">Chocolate cake apple pie powder brownie cupcake pastry gummies cupcake. Icing brownie pie chupa chups tart pie bonbon lemon drops. I love sweet marshmallow I love jelly croissant.</p>
