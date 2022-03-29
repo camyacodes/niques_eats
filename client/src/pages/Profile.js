215ed3c8ddc6970b488b9b79f68f0e6cdc4d12b1
camyacodes <camyacodes@gmail.com>
2022-03-28 22:07:39 -0400

    test 2 branch

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,27 +1,36 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { QUERY_USERS } from '../utils/queries';
 
 
 const Profile = () => {
-  const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(QUERY_USER, {
-    variables: { username: userParam }
-  });
+  const { data, loading, error } = useQuery(QUERY_USERS);
 
-  const user = data?.user || {};
+  // const { username: userParam } = useParams();
+
+  // const { loading, data } = useQuery(QUERY_USER, {
+  //   variables: { username: userParam }
+  // });
+
+  // const user = data?.user || {};
 
   if (loading) {
     return <div>Loading...</div>;
   }
+  if (error) return <pre>{error.message}</pre>
 
   return (
    
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {user.username}'s profile.
+        {/* Viewing {user.username}'s profile. */}
+        <ul>
+        {data.users.map((user) => (
+          <li key={user.id}>{user.username}</li>
+        ))}
+      </ul>
         </h2>
       </div>
    
