25373335d98c8735b60b16c2d5ae2eed0ede7ecd
chaudeveloper <ms_michanguyen@yahoo.com>
2022-03-29 23:53:38 -0400

    delete useCreateIndex and useFindAndModify from connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://c-square:c-square@cluster0.eywg7.mongodb.net/niques-eats?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
 });
 
 module.exports = mongoose.connection;
