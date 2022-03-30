484db2047b7c579fecb75fb51db61572817b816e
chaudeveloper <ms_michanguyen@yahoo.com>
2022-03-30 02:54:56 -0400

    another effort in server.js

--- server/server.js
+++ server/server.js
@@ -33,13 +33,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 app.get('/api', (req, res) => {
   res.send('Hello World!')
 })
+
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
