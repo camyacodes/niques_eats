58553f3f9964b146c8a349ad3e8272852b1a4ef2
chaudeveloper <ms_michanguyen@yahoo.com>
2022-03-29 23:35:32 -0400

    update mongodb link in connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/niques-eats', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://c-square:c-square@cluster0.eywg7.mongodb.net/niques-eats?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
