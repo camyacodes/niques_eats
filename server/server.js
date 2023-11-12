03fa597914c1dc39e494e8659eddae5e2bbf0071
camyacodes <camyacodes@gmail.com>
2023-11-11 22:16:59 -0500

    changed port

--- server/server.js
+++ server/server.js
@@ -35,8 +35,8 @@ async function startApolloServer() {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 
-  await new Promise(resolve => app.listen({ port: 4000 }, resolve));
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  await new Promise(resolve => app.listen({ port: 3001 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:3001${server.graphqlPath}`);
   return { server, app };
 }
 
