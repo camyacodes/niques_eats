caa19a68337b8fb7ad453714ac2b6114dd773090
camyacodes <camyacodes@gmail.com>
2023-11-11 19:09:35 -0500

    added build script

---
+++ client/build/precache-manifest.e3c67d9023e6c8b70037963733ca0805.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742d17966096102a63a3ac3bdaa58cfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50e498a507d995c49ee5",
+    "url": "/static/css/2.d9ec06b4.chunk.css"
+  },
+  {
+    "revision": "12d9877c5e6733ec3891",
+    "url": "/static/css/main.837f015b.chunk.css"
+  },
+  {
+    "revision": "50e498a507d995c49ee5",
+    "url": "/static/js/2.b74fe278.chunk.js"
+  },
+  {
+    "revision": "c21d579849576bf6a47550572ff70f62",
+    "url": "/static/js/2.b74fe278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12d9877c5e6733ec3891",
+    "url": "/static/js/main.4d26e598.chunk.js"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "802eaa68c1742c9729be888f90f72201",
+    "url": "/static/media/brunch-menu-background.802eaa68.png"
+  },
+  {
+    "revision": "df2648abccd87914f1d75cdbf0718656",
+    "url": "/static/media/dinner-menu-background.df2648ab.png"
+  },
+  {
+    "revision": "ff105d6e3cf08efae8ec997998545758",
+    "url": "/static/media/profile2.ff105d6e.jpg"
+  },
+  {
+    "revision": "71a95aecde182b9b9774be732f6e7a87",
+    "url": "/static/media/salmon_meal.71a95aec.jpg"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
